a13594769edd815c00a6f5516a4821716de6453f
Sarp <sarppehlivan444@gmail.com>
2026-08-09 08:03:29 +0300

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n: number): string[] {
+        const result: string[] = [];
+        const backtrack = (path: string[], open: number, close: number) => {
+            if (path.length === n * 2) {
+                result.push([...path].join(""));
+                return;
+            }
+
+            if (open < n) {
+                path.push("(")
+                backtrack(path, open + 1, close);
+                path.pop();
+                
+                backtrack(path, open + 1, close);
+
+            }
+            
+            if (close < open) {
+                path.push(")");
+                backtrack(path, open, close + 1);
+                path.pop();
+
+                backtrack(path, open, close + 1);
+            }
+        }
+
+        backtrack([], 0, 0)
+        return result;
+    }
+}
