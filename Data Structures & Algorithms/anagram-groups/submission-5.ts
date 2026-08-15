da3879fd0d1e54acd22f2242846f4a739669b28f
Sarp <sarppehlivan444@gmail.com>
2026-08-15 08:28:29 +0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const hash = new Map<string, string[]>();
+        const result: string[][] = [];
+        for (const string of strs) {
+            const key = [...string].sort().join("");
+            if (!hash.has(key)) {
+                hash.set(key, [string]);
+                continue;
+            }
+
+            hash.get(key).push(string);
+        }
+
+        return [...hash.values()];
+    }
+}
