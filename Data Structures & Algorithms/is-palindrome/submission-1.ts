24d2c3e42039b24a97d34aa0b5911c6bfd9374fd
Sarp <sarppehlivan444@gmail.com>
2026-08-16 08:07:37 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const lowerLetters = "qwertyuopasdfghjklizxcvbnm";
+        const upperLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        const numberLetters = "0123456789"
+        let word = "";
+        
+        for (const string of s) {
+            if (lowerLetters.includes(string)) {
+                word += string;
+            }
+
+            if (upperLetters.includes(string)) {
+                word += string.toLowerCase();
+            }
+
+            if (numberLetters.includes(string)) {
+                word += string;
+            }
+        }
+
+        let left = 0;
+        let right = word.length - 1;
+
+        console.log("word: ", word);
+
+        while (left < right) {
+            console.log("left: ", )
+            if (word[left] !== word[right]) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
