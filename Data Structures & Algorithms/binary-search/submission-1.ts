68b0fc7fdf8b5c6d3418f1665ef2cd5d959b6127
Sarp <sarppehlivan444@gmail.com>
2026-08-18 13:48:01 +0300

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            console.log("left: ", nums[left]);
+            console.log("mid: ", nums[mid]);
+            console.log("right: ", nums[right]);
+
+            if (nums[mid] === target) return mid;
+            
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
